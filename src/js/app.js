9903bd546ad53a5a29c44bff5c9b8462ec7afec4
scherpablo <94416638+scherpablo@users.noreply.github.com>
2022-10-20 20:23:50 -0300

    App.js

--- src/js/app.js
+++ src/js/app.js
@@ -116,8 +116,8 @@ async function consultarAPI() {
     try {   
         // const url = `${location.origin}/api/servicios`;
         // const url = 'http://localhost:3000/api/servicios';
-        // const url = 'https://facilitadora-app-de-turnos.herokuapp.com//api/servicios';
-        const url = `${server}/api/servicios`;
+        const url = 'https://facilitadora-app-de-turnos.herokuapp.com//api/servicios';
+        // const url = `${server}/api/servicios`;
         const resultado = await fetch(url); //Funcion que nos permite consumir la url
         const servicios = await resultado.json(); //Obtenemos los resultados como json
 
@@ -312,8 +312,8 @@ async function reservarTurno() {
         // Peticion a la API
     // const url = `${location.origin}/api/citas`;
     // const url = 'http://localhost:3000/api/citas';
-    // const url = 'https://facilitadora-app-de-turnos.herokuapp.com//api/citas';
-    const url = `${server}/api/citas`;
+    const url = 'https://facilitadora-app-de-turnos.herokuapp.com//api/citas';
+    // const url = `${server}/api/citas`;
 
     const respuesta = await fetch(url, {
         method: 'POST',
