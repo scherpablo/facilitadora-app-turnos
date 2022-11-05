3377aa55dc44057428ec4a97d426be54cb543763
Pablo Scherpa <scherpablo@gmail.com>
2022-11-05 11:29:18 -0300

    Update crearCuenta.php

--- views/auth/crearCuenta.php
+++ views/auth/crearCuenta.php
@@ -3,18 +3,6 @@
 
 <?php
     include_once __DIR__ . '/../tamplates/alertas.php';
-
-    $ip = $_SERVER['REMOTE_ADDR'];
-    $captcha =  $_POST['g-recaptcha-response'];
-    $secretkey = '6LeOwN8iAAAAAPxAZRj4Xfa_EWxcmA6tM52t6V4q';
-
-    $respuesta = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret=$secretkey&response=$captacha&remoteip=$ip');
-
-    $atributos = json_decode($respuesta, TRUE);
-
-    if (!$atributos['success']) {
-        self::$alertas['error'][] = 'Verifica el Captcha';
-    }
 ?>
 
 <form action="/crearCuenta" class="formulario" method="POST">
@@ -58,5 +46,19 @@
     <a href="/olvide">¿Olvidaste tu password?</a>
 
 </div>
+
+<?php
+$ip = $_SERVER['REMOTE_ADDR'];
+$captcha =  $_POST['g-recaptcha-response'];
+$secretkey = '6LeOwN8iAAAAAPxAZRj4Xfa_EWxcmA6tM52t6V4q';
+
+$respuesta = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret=$secretkey&response=$captacha&remoteip=$ip');
+
+$atributos = json_decode($respuesta, TRUE);
+
+if (!$atributos['success']) {
+    self::$alertas['error'][] = 'Verifica el Captcha';
+}
+?>
 
 <script src="https://www.google.com/recaptcha/api.js" async defer></script>
