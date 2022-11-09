e28faca7af17c25c4d25c0726af7070e13c15628
Pablo Scherpa <scherpablo@gmail.com>
2022-11-08 21:59:33 -0300

    Railway

--- includes/app.php
+++ includes/app.php
@@ -7,7 +7,6 @@ $dotenv->safeLoad();
 require 'funciones.php';
 require 'database.php';
 
-
 // Conectarnos a la base de datos
 use Model\ActiveRecord;
 ActiveRecord::setDB($db);
--- src/js/app.js
+++ src/js/app.js
@@ -116,8 +116,8 @@ const server = window.location.origin; //Contsnate de server que utilizamos lueg
 async function consultarAPI() {
     
     try {   
-        // const url = `${location.origin}/api/servicios`; //URL para proyecyo en produccion (HEROKU)
-        const url = 'http://localhost:3000/api/servicios'; //URL para proyecyo en desarrollo
+        const url = `${location.origin}/api/servicios`; //URL para proyecyo en produccion (HEROKU)
+        // const url = 'http://localhost:3000/api/servicios'; //URL para proyecyo en desarrollo
         // const url = 'mysql://${{ MYSQLUSER }}:${{ MYSQLPASSWORD }}@${{ MYSQLHOST }}:${{ MYSQLPORT }}/${{ MYSQLDATABASE }}'; 
         
         const resultado = await fetch(url); //Funcion que nos permite consumir la url
@@ -310,8 +310,8 @@ async function reservarTurno() {
 
     try {
         // Peticion a la API
-    // const url = `${location.origin}/api/citas`; //URL para proyecyo en produccion (HEROKU)
-    const url = 'http://localhost:3000/api/citas'; //URL para proyecyo en desarrollo  
+    const url = `${location.origin}/api/citas`; //URL para proyecyo en produccion (HEROKU)
+    // const url = 'http://localhost:3000/api/citas'; //URL para proyecyo en desarrollo  
     // const url = 'mysql://${{ MYSQLUSER }}:${{ MYSQLPASSWORD }}@${{ MYSQLHOST }}:${{ MYSQLPORT }}/${{ MYSQLDATABASE }}';  
 
     const respuesta = await fetch(url, {
