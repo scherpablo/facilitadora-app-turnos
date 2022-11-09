1044760a29816ec44adf316c3955d2955f5bbb54
Pablo Scherpa <scherpablo@gmail.com>
2022-11-09 10:43:05 -0300

    Railway

--- src/js/app.js
+++ src/js/app.js
@@ -116,8 +116,8 @@ const server = window.location.origin; //Contsnate de server que utilizamos lueg
 async function consultarAPI() {
     
     try {   
-        const url = `${location.origin}/api/servicios`; //URL para proyecyo en produccion (HEROKU)
-        // const url = 'http://localhost:3000/api/servicios'; //URL para proyecyo en desarrollo
+        // const url = `${location.origin}/api/servicios`; //URL para proyecyo en produccion (HEROKU)
+        const url = 'http://localhost:3000/api/servicios'; //URL para proyecyo en desarrollo
         // const url = 'mysql://${{ MYSQLUSER }}:${{ MYSQLPASSWORD }}@${{ MYSQLHOST }}:${{ MYSQLPORT }}/${{ MYSQLDATABASE }}'; 
         
         const resultado = await fetch(url); //Funcion que nos permite consumir la url
